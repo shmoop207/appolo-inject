4b8ac93e71ba16c48c854ca2b77e89ea271cfd71
roman <shmoop207@gmail.com>
2014-02-22 23:41:08 +0200

    added tests

--- test/ioc.js
+++ test/ioc.js
@@ -108,4 +108,119 @@ describe('Ioc', function () {
 
         });
     });
+
+
+    describe('reset ioc', function () {
+        var injector;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+            var Rectangle = Class.define({
+
+                constructor: function () {
+
+                }
+            });
+
+            injector.addDefinitions({
+                rectangle: {
+                    type: Rectangle
+                }
+            });
+
+            injector.initialize();
+        });
+
+
+        it('should reset ioc', function () {
+
+
+            should.exist(injector.getDefinition('rectangle'));
+            should.exist(injector.getObject('rectangle'));
+
+            injector.reset();
+            should.not.exist(injector.getDefinition('rectangle'));
+            (function () {
+                var rectangle = injector.getObject('rectangle');
+            }).should.throw()
+
+        });
+    });
+
+    describe('add object', function () {
+        var injector;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+
+            injector.initialize();
+        });
+
+
+        it('should add object', function () {
+
+            function Test() {
+            }
+
+            injector.addObject('test', new Test());
+
+            var test = injector.getObject('test');
+
+            should.exist(test);
+
+            test.should.be.an.instanceOf(Test)
+
+        });
+    });
+
+    describe('get object by type', function () {
+        var injector, Rectangle, Circle;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+            Rectangle = Class.define({
+
+                constructor: function () {
+
+                }
+            });
+
+            Circle = Class.define({
+
+                constructor: function () {
+
+                }
+            });
+
+            injector.addDefinitions({
+                rectangle: {
+                    type: Rectangle,
+                    singleton:true
+                }
+            });
+
+            injector.addDefinitions({
+                circle: {
+                    type: Circle,
+                    singleton:true
+                }
+            });
+
+            injector.initialize();
+        });
+
+        it('should get by type', function () {
+
+            var objects= injector.getObjectsByType(Rectangle);
+
+            objects.should.be.instanceof(Array).and.have.lengthOf(1);
+
+            objects[0].should.be.instanceof(Rectangle);
+        });
+    });
+
+
 });
