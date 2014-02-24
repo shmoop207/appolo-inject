ecf8f7e261475a38bb78df16b0f14129f1896607
roman <shmoop207@gmail.com>
2014-02-24 17:38:05 +0200

    fix factory get object

--- test/factory.js
+++ test/factory.js
@@ -115,6 +115,7 @@ describe('Property Factory', function () {
 
 
 
+
             injector.addDefinitions({
                 rectangle: {
                     type: Rectangle,
@@ -151,6 +152,170 @@ describe('Property Factory', function () {
             rectangle.manager.should.be.instanceof(FooManager);
         });
     });
+
+
+    describe('inject factory Object', function () {
+        var injector,LocalFooManager;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+            LocalFooManager = Class.define({
+
+                constructor: function () {
+                    this.name = 'foo';
+                }
+            });
+
+            var FooManagerFactory = Class.define({
+
+                constructor: function () {
+
+                },
+
+                get:function(){
+                    return this.localFooManager;
+                }
+            });
+
+            injector.addDefinitions({
+
+                localFooManager: {
+                    type: LocalFooManager,
+                    singleton: true
+
+                },
+                fooManagerFactory: {
+                    type: FooManagerFactory,
+                    singleton: true,
+                    inject:['localFooManager']
+
+                }
+            });
+
+            injector.initialize();
+        });
+
+        it('should inject object with get object', function () {
+
+            var fooManager = injector.getObject('fooManager');
+
+            should.exist(fooManager);
+
+            fooManager.should.be.instanceof(LocalFooManager);
+
+            fooManager.name.should.be.equal("foo");
+        });
+    });
+
+    describe('inject 2 factories', function () {
+        var injector,LocalFooManager,RemoteBarManager;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+            LocalFooManager = Class.define({
+
+                constructor: function () {
+                    this.name = 'foo';
+                }
+            });
+
+            RemoteBarManager = Class.define({
+
+                constructor: function () {
+                    this.name = 'bar';
+                }
+            });
+
+            var FooManagerFactory = Class.define({
+
+                constructor: function () {
+
+                },
+
+                get:function(){
+                    return this.localFooManager;
+                }
+            });
+
+            var BarManagerFactory = Class.define({
+
+                constructor: function () {
+
+                },
+
+                get:function(){
+                    return this.remoteBarManager;
+                }
+            });
+
+            var Rectangle = Class.define({
+
+                constructor: function () {
+
+                },
+                getName: function () {
+
+                    return this.fooManager.name;
+                },
+                getName2: function () {
+
+                    return this.barManager.name;
+                }
+            });
+
+
+            injector.addDefinitions({
+
+                localFooManager: {
+                    type: LocalFooManager,
+                    singleton: true
+
+                },
+                remoteBarManager: {
+                    type: RemoteBarManager,
+                    singleton: true
+
+                },
+                fooManagerFactory: {
+                    type: FooManagerFactory,
+                    singleton: true,
+                    inject:['localFooManager']
+
+                },
+                barManagerFactory: {
+                    type: BarManagerFactory,
+                    singleton: true,
+                    inject:['remoteBarManager']
+                },
+                rectangle: {
+                    type: Rectangle,
+                    singleton: true,
+                    inject:['barManager','fooManager']
+                }
+            });
+
+            injector.initialize();
+        });
+
+        it('should inject object with get object', function () {
+
+            var rectangle = injector.getObject('rectangle');
+
+            should.exist(rectangle);
+
+            should.exist(rectangle.fooManager);
+            should.exist(rectangle.barManager);
+
+            rectangle.fooManager.should.be.instanceof(LocalFooManager);
+            rectangle.barManager.should.be.instanceof(RemoteBarManager);
+
+            rectangle.getName().should.be.equal("foo");
+            rectangle.getName2().should.be.equal("bar");
+        });
+    });
+
 
 });
 
--- test/factoryMethod.js
+++ test/factoryMethod.js
@@ -62,6 +62,77 @@ describe('Property Factory Method', function () {
 
 
         });
+
+
+    });
+
+    describe('inject factory method with args', function () {
+        var injector,FooManager;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+            var Rectangle = Class.define({
+
+                constructor: function () {
+
+                },
+                getName: function (name) {
+
+                    return this.createFooManager(name).getName();
+                }
+
+            });
+
+            FooManager = Class.define({
+
+                constructor: function (name) {
+                    this.name = name;
+                },
+
+                getName:function(){
+                    return this.name;
+                }
+            });
+
+
+
+            injector.addDefinitions({
+                rectangle: {
+                    type: Rectangle,
+                    singleton: false,
+                    properties: [
+                        {
+                            name: 'createFooManager',
+                            factoryMethod: 'fooManager'
+                        }
+                    ]
+                },
+                fooManager: {
+                    type: FooManager,
+                    singleton: false
+                }
+            });
+
+            injector.initialize();
+        });
+
+        it('should inject factory method that creates objects and call object with args', function () {
+
+            var rectangle = injector.getObject('rectangle');
+
+            should.exist(rectangle.createFooManager);
+
+            rectangle.createFooManager.should.be.a('Function')
+
+            rectangle.createFooManager().should.be.instanceof(FooManager);
+
+            should.exist(rectangle.createFooManager('test').name);
+
+            rectangle.createFooManager('test').name.should.be.equal("test");
+
+            rectangle.getName("test2").should.be.equal("test2")
+        });
     });
 
 });
