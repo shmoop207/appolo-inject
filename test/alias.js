66521b517b6138e8c63071be2593d4c65b84c123
roman <shmoop207@gmail.com>
2014-04-03 12:27:07 +0300

    travis

--- test/alias.js
+++ test/alias.js
@@ -32,7 +32,7 @@ describe('Alias', function () {
             injector.addDefinitions({
                 rectangle: {
                     type: Rectangle,
-                    singleton: false,
+                    singleton: true,
                     props: [
                         {
                             name: 'calcable',
@@ -91,7 +91,7 @@ describe('Alias', function () {
     });
 
     describe('should inject multi alias', function () {
-        var injector,CalcManager;
+        var injector,CalcManager,FooManager;
 
         beforeEach(function () {
             injector = inject.createContainer();
@@ -181,6 +181,114 @@ describe('Alias', function () {
 
         });
     });
+
+
+
+
+
+    describe('should inject multi alias by class type', function () {
+        var injector,CalcManager,FooManager;
+
+        beforeEach(function () {
+            injector = inject.createContainer();
+
+
+            var Cleanable = Class.define({
+
+                constructor: function () {
+
+                }
+            });
+
+
+            var Rectangle = Class.define({
+
+                constructor: function () {
+
+                }
+            });
+
+            CalcManager = Class.define({
+
+                constructor: function () {
+
+                },
+
+                calc: function () {
+                    return 25
+                }
+            });
+
+            FooManager = Class.define({
+
+                constructor: function () {
+
+                },
+
+                calc: function () {
+                    return 25
+                },
+
+                cleanable:function(){
+
+                }
+            });
+
+            injector.addDefinitions({
+                rectangle: {
+                    type: Rectangle,
+                    singleton: false,
+                    props: [
+                        {
+                            name: 'calcable',
+                            alias: 'calcable'
+                        },
+                        {
+                            name: 'cleanable',
+                            alias: Cleanable
+                        }
+                    ]
+                },
+                calcManager: {
+                    type: CalcManager,
+                    alias:['calcable'],
+                    singleton: true
+                },
+
+                fooManager: {
+                    type: FooManager,
+                    alias:['calcable',Cleanable],
+                    singleton: true
+                },
+
+                barManager: {
+                    type: CalcManager,
+                    alias:['calcable'],
+                    singleton: true
+                }
+            });
+
+            injector.initialize();
+        });
+
+        it('should inject property ', function () {
+
+            var rectangle = injector.getObject('rectangle');
+
+            should.exist(rectangle.calcable);
+            should.exist(rectangle.cleanable);
+
+            rectangle.calcable.should.be.an.instanceOf(Array);
+            rectangle.cleanable.should.be.an.instanceOf(Array);
+
+            rectangle.calcable.length.should.be.equal(3);
+            rectangle.cleanable.length.should.be.equal(1);
+
+        });
+    });
+
+
+
 
 
 
