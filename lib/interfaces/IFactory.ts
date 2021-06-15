ac7c143fdbc8860f4aae14576b884b51213e9c8f
roman <shmoop207@gmail.com>
2021-06-15 15:02:56 +0300

    added factory type to interface

--- lib/interfaces/IFactory.ts
+++ lib/interfaces/IFactory.ts
@@ -2,4 +2,4 @@ export interface IFactory<T> {
     get(): T | Promise<T>
 }
 
-export type FactoryFn<T extends new (...args: any) => any> = (...args: ConstructorParameters<T>) => InstanceType<T>
+export type FactoryFn<T extends new (...args: any) => any> = ((...args: ConstructorParameters<T>) => InstanceType<T>) & {type:T}
